0a1cf53faf56ddcc2e57c5ee3e3a30ff52593d44
Ahmet Esad Barut <dwaebtdt@gmail.com>
2022-02-15 21:57:25 +0300

    Update Command

--- WebApi/BookOperations/CreateBook/CreateBookCommand.cs
+++ WebApi/BookOperations/CreateBook/CreateBookCommand.cs
@@ -1,22 +1,24 @@
 using System;
 using System.Linq;
+using AutoMapper;
 using WebApi.DBOperations;
 
 namespace WebApi.BookOperations.CreateBook{
     public class CreateBookCommand{
         public CreateBookModel Modal { get; set; }
         private readonly BookStoreDbContext _dbContext;
-        public CreateBookCommand(BookStoreDbContext dbContext) => _dbContext = dbContext;
+        private readonly IMapper _mapper;
+        public CreateBookCommand(BookStoreDbContext dbContext, IMapper mapper) {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
 
         public void Handle(){
             var book = _dbContext.Books.SingleOrDefault(x => x.Title == Modal.Title);
             if(book is not null) throw new InvalidOperationException("Kitap Mevcut");
-            book = new Book();
-            book.Title = Modal.Title;
-            book.PublishDate = Modal.PublishDate;
-            book.PageCount = Modal.PageCount;
-            book.GenreId = Modal.GenreId;
 
+            book = _mapper.Map<Book>(Modal);
+            
             _dbContext.Books.Add(book);
             _dbContext.SaveChanges();
         }
